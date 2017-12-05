ee0f8b8370f0cfaea677fa48663a9c03f9a77718
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-12-05 09:22:08 +0100

    Add solutions for day 5

---
+++ 5.dart
@@ -0,0 +1,23 @@
+import 'utils.dart';
+
+void main() {
+  // part 1
+  final data = read('5.txt').trim().split('\n').map(int.parse).toList();
+  final data1 = data.toList();
+  var ptr = 0, counter = 0;
+  while (ptr >= 0 && ptr < data1.length) {
+    counter++;
+    ptr += data1[ptr]++;
+  }
+  print(counter);
+
+  // part 2
+  final data2 = data.toList();
+  ptr = 0;
+  counter = 0;
+  while (ptr >= 0 && ptr < data2.length) {
+    counter++;
+    ptr += data2[ptr] >= 3 ? data2[ptr]-- : data2[ptr]++;
+  }
+  print(counter);
+}
