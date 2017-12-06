82ddaebb126ebd7b182a982378357a691107eb9c
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-12-06 09:28:33 +0100

    Add solutions for day 6

---
+++ 6.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+import 'utils.dart';
+
+void main() {
+  // part 1
+  final List<int> data =
+      read('6.txt').trim().split(new RegExp(r'\s')).map(int.parse).toList();
+  final history = new List<List<int>>();
+  while (!history.any((list) => compareLists(list, data))) {
+    history.add(data.toList());
+    var i = data.indexOf(data.reduce(max));
+    var redist = data[i];
+    data[i] = 0;
+    while (redist-- > 0) {
+      data[++i % data.length]++;
+    }
+  }
+  print(history.length);
+
+  // part 2.
+  print(history.length -
+      history.indexOf(history.singleWhere((list) => compareLists(list, data))));
+}
+
+bool compareLists(List a, List b) {
+  for (var i = 0; i < a.length; i++) {
+    if (b.length <= i || a[i] != b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
